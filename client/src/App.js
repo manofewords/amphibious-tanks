f577b58874922c17733684a52d92fe67a5f90ed1
Manuel Hitz <manuel.hitz@gmail.com>
2017-11-09 10:58:34 +0100

    display a Mapbox map with all vehicles as simple dots

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
+import ReactMapGL, {Marker} from 'react-map-gl';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      response: false,
-      endpoint: "http://127.0.0.1:4001"
+      vehicles: []
     };
   }
 
   componentDidMount() {
-    const { endpoint } = this.state;
-    const socket = socketIOClient(endpoint);
-    socket.on("vehicles-status", data => this.setState({ response: data }));
+    const socket = socketIOClient("http://127.0.0.1:4001");
+    socket.on("vehicles-status", data => this.setState({ vehicles: data }));
   }
 
   render() {
-    const { response } = this.state;
-    console.log(response);
+    const { vehicles } = this.state;
+
+    var markers = vehicles.map(function(vehicle) {
+      return <Marker latitude={vehicle.position[1]} longitude={vehicle.position[0]} key={vehicle.id}>
+        <div>•</div>
+        </Marker>;
+    });
+
     return (
-      <div>
-      </div>
+      <ReactMapGL
+        mapboxApiAccessToken="pk.eyJ1IjoibWFub2Zld29yZHMiLCJhIjoiY2o5c2ExZDQ1NjAyaDJxcXNtbzBjY2FjOSJ9.deGZaKnb9EoJKVl969U-HA"
+        width={400}
+        height={400}
+        latitude={37.7577}
+        longitude={-122.4376}
+        zoom={8}>
+        { markers }
+      </ReactMapGL>
     );
   }
 }
