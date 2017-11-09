bb9e0ccdb852dc68b26e5813e4e27d6ef5877731
Manuel Hitz <manuel.hitz@gmail.com>
2017-11-09 14:25:39 +0100

    move the fleet statistics into a component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 import ReactMapGL, {Marker} from "react-map-gl";
+import FleetStatistics from "./components/FleetStatistics";
 import SearchableVehicleDetails from "./components/SearchableVehicleDetails";
 
 class App extends Component {
@@ -55,58 +56,6 @@ class App extends Component {
         </li>;
     });
 
-    const PERCENTAGE_STATS_TYPE = "percentage";
-    const NUMBER_STATS_TYPE = "number";    
-
-    var fleetStatistics = function() {
-      var statistics = [],
-        occupancyRatio = 0,
-        travelerTotal = 0,
-        fleetSize = vehicles.length,
-        vehicleStatusTotal = {
-          INACTIVE: 0,
-          IDLE: 0, 
-          ACTIVE: 0, 
-          ERROR: 0
-        };
-
-      vehicles.forEach(function(vehicle) {
-        occupancyRatio += (vehicle.occupancy / vehicle.max_occupancy);
-        travelerTotal += vehicle.occupancy;
-        vehicleStatusTotal[vehicleStatus[vehicle.status.id]] += 1;
-      });
-
-      statistics.push({
-        id: "occupancyRatio",
-        description: "occupancy ratio of the vehicles",
-        value: occupancyRatio / fleetSize,
-        type: PERCENTAGE_STATS_TYPE
-      }, {
-        id: "travelerTotal",
-        description: "nb of travelers the fleet is serving",
-        value: travelerTotal,
-        type: NUMBER_STATS_TYPE
-      });
-
-      for(var status in vehicleStatusTotal) {
-        statistics.push({
-          id: "statusRatio" + status,
-          description: "ratio of vehicles in " + status + " state",
-          value: vehicleStatusTotal[status] / fleetSize,
-          type: PERCENTAGE_STATS_TYPE
-        });
-      }
-
-      return statistics;
-    };
-
-    var statisticsRow = fleetStatistics().map(function(statistic) {
-      return <tr key={ statistic.id }>
-          <td>{ statistic.description }</td>
-          <td>{ statistic.type === PERCENTAGE_STATS_TYPE ? + Math.round(statistic.value * 100) + "%" : statistic.value }</td>
-        </tr>;
-    });
-
     const ACCESS_TOKEN = "pk.eyJ1IjoibWFub2Zld29yZHMiLCJhIjoiY2o5c2ExZDQ1NjAyaDJxcXNtbzBjY2FjOSJ9.deGZaKnb9EoJKVl969U-HA";
 
     return (
@@ -129,14 +78,7 @@ class App extends Component {
           </figcaption>
         </figure>
 
-        <section>
-          Fleet statistics:
-          <table>
-            <tbody>
-            { statisticsRow }
-            </tbody>
-          </table>
-        </section>
+        <FleetStatistics vehicles={ vehicles }/>
 
         <SearchableVehicleDetails vehicles={ vehicles }/>
       </div>
---
+++ client/src/components/FleetStatistics.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+const vehicleStatus = ["INACTIVE", "IDLE", "ACTIVE", "ERROR"];
+
+const PERCENTAGE_STATS_TYPE = "percentage";
+const NUMBER_STATS_TYPE = "number";   
+
+class FleetStatistics extends Component {  
+  calculateStatistics(vehicles) {
+    var statistics = [],
+      occupancyRatio = 0,
+      travelerTotal = 0,
+      fleetSize = vehicles.length,
+      vehicleStatusTotal = {
+        INACTIVE: 0,
+        IDLE: 0, 
+        ACTIVE: 0, 
+        ERROR: 0
+      };
+
+    vehicles.forEach(function(vehicle) {
+      occupancyRatio += (vehicle.occupancy / vehicle.max_occupancy);
+      travelerTotal += vehicle.occupancy;
+      vehicleStatusTotal[vehicleStatus[vehicle.status.id]] += 1;
+    });
+
+    statistics.push({
+      id: "occupancyRatio",
+      description: "occupancy ratio of the vehicles",
+      value: occupancyRatio / fleetSize,
+      type: PERCENTAGE_STATS_TYPE
+    }, {
+      id: "travelerTotal",
+      description: "nb of travelers the fleet is serving",
+      value: travelerTotal,
+      type: NUMBER_STATS_TYPE
+    });
+
+    for(var status in vehicleStatusTotal) {
+      statistics.push({
+        id: "statusRatio" + status,
+        description: "ratio of vehicles in " + status + " state",
+        value: vehicleStatusTotal[status] / fleetSize,
+        type: PERCENTAGE_STATS_TYPE
+      });
+    }
+
+    return statistics;
+  }
+
+  render() {
+    const vehicles = this.props.vehicles;
+
+    if(!vehicles || vehicles.length === 0) return null;
+
+    const statistics = this.calculateStatistics(vehicles);
+
+    var statisticsRow = statistics.map(function(statistic) {
+      return <tr key={ statistic.id }>
+          <td>{ statistic.description }</td>
+          <td>{ statistic.type === PERCENTAGE_STATS_TYPE ? + Math.round(statistic.value * 100) + "%" : statistic.value }</td>
+        </tr>;
+    });
+
+    return (
+      <section>
+        <h1>Fleet statistics</h1>
+        <table>
+          <tbody>
+          { statisticsRow }
+          </tbody>
+        </table>
+      </section>
+    );
+  }
+}
+
+export default FleetStatistics;
