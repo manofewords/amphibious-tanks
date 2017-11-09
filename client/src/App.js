34f69577e2560b3583442642d55b67164d033634
Manuel Hitz <manuel.hitz@gmail.com>
2017-11-09 13:02:46 +0100

    display some fleet statistics

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,8 @@ class App extends Component {
   render() {
     const { vehicles } = this.state;
 
+    if(!vehicles || vehicles.length === 0) return null;
+
     const vehicleStatus = ["INACTIVE", "IDLE", "ACTIVE", "ERROR"];
 
     var vehicleStyle = function(status, bearing) {
@@ -52,24 +54,87 @@ class App extends Component {
         </li>;
     });
 
+    const PERCENTAGE_STATS_TYPE = "percentage";
+    const NUMBER_STATS_TYPE = "number";    
+
+    var fleetStatistics = function() {
+      var statistics = [],
+        occupancyRatio = 0,
+        travelerTotal = 0,
+        fleetSize = vehicles.length,
+        vehicleStatusTotal = {
+          INACTIVE: 0,
+          IDLE: 0, 
+          ACTIVE: 0, 
+          ERROR: 0
+        };
+
+      vehicles.forEach(function(vehicle) {
+        occupancyRatio += (vehicle.occupancy / vehicle.max_occupancy);
+        travelerTotal += vehicle.occupancy;
+        vehicleStatusTotal[vehicleStatus[vehicle.status.id]] += 1;
+      });
+
+      statistics.push({
+        id: "occupancyRatio",
+        description: "occupancy ratio of the vehicles",
+        value: occupancyRatio / fleetSize,
+        type: PERCENTAGE_STATS_TYPE
+      }, {
+        id: "travelerTotal",
+        description: "nb of travelers the fleet is serving",
+        value: travelerTotal,
+        type: NUMBER_STATS_TYPE
+      });
+
+      for(var status in vehicleStatusTotal) {
+        statistics.push({
+          id: "statusRatio" + status,
+          description: "ratio of vehicles in " + status + " state",
+          value: vehicleStatusTotal[status] / fleetSize,
+          type: PERCENTAGE_STATS_TYPE
+        });
+      }
+
+      return statistics;
+    };
+
+    var statisticsRow = fleetStatistics().map(function(statistic) {
+      return <tr key={ statistic.id }>
+          <td>{ statistic.description }</td>
+          <td>{ statistic.type === PERCENTAGE_STATS_TYPE ? + Math.round(statistic.value * 100) + "%" : statistic.value }</td>
+        </tr>;
+    });
+
     return (
-      <figure>
-        <ReactMapGL
-          mapboxApiAccessToken="pk.eyJ1IjoibWFub2Zld29yZHMiLCJhIjoiY2o5c2ExZDQ1NjAyaDJxcXNtbzBjY2FjOSJ9.deGZaKnb9EoJKVl969U-HA"
-          width={400}
-          height={400}
-          latitude={37.7577}
-          longitude={-122.4376}
-          zoom={8}>
-          { markers }
-        </ReactMapGL>
-        <figcaption>
-          Vehicle status: 
-          <ul>
-            { statusLegends }
-          </ul>
-        </figcaption>
-      </figure>
+      <div>
+        <figure>
+          <ReactMapGL
+            mapboxApiAccessToken="pk.eyJ1IjoibWFub2Zld29yZHMiLCJhIjoiY2o5c2ExZDQ1NjAyaDJxcXNtbzBjY2FjOSJ9.deGZaKnb9EoJKVl969U-HA"
+            width={400}
+            height={400}
+            latitude={37.7577}
+            longitude={-122.4376}
+            zoom={8}>
+            { markers }
+          </ReactMapGL>
+          <figcaption>
+            Vehicle status: 
+            <ul>
+              { statusLegends }
+            </ul>
+          </figcaption>
+        </figure>
+
+        <section>
+          Fleet statistics:
+          <table>
+            <tbody>
+            { statisticsRow }
+            </tbody>
+          </table>
+        </section>
+      </div>
     );
   }
 }
