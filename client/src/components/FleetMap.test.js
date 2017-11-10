1a042469b18a16e4f50cd633fef29b1127a9a01f
Manuel Hitz <manuel.hitz@gmail.com>
2017-11-10 10:38:04 +0100

    add tests for the FleetMap component
    only “business logic” tests for now, no rendering tests

---
+++ client/src/components/FleetMap.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FleetMap from "./FleetMap";
+import { shallow } from "enzyme";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<FleetMap />, div);
+});
+
+it("handles viewport changes", () => {
+  const newViewport = {
+    width: 1,
+    height: 2,
+    latitude: 3,
+    longitude: 4,
+    zoom: 5
+  };
+
+  const wrapper = shallow(<FleetMap />);
+
+  // default value
+  expect(wrapper.state().viewport).toEqual({
+    width: 400,
+    height: 400,
+    latitude: 37.755705,
+    longitude: -122.447177,
+    zoom: 8
+  });
+
+  // viewport has changed (e.g. through user interaction)
+  wrapper.instance().handleMapViewportChange(newViewport);
+  expect(wrapper.state().viewport).toEqual(newViewport);
+});
